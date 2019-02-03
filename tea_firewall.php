c6cd3b146ecddb3673d2818c33f2eb71441b8fa7
Ammar Faizi <ammarfaizi2@gmail.com>
2019-02-03 10:07:51 +0700

    Implements iptables

--- tea_firewall.php
+++ tea_firewall.php
@@ -64,7 +64,7 @@ if (!($workerPid = pcntl_fork())) {
 					$curData = [$m[1]];
 				}
 				unset($r);
-
+				$curData = array_unique($curData);
 				shmop_write($shmid, str_to_nts(json_encode($curData)), 0);
 				shmop_close($shmid);
 			}
@@ -85,6 +85,8 @@ if (!($blockerPid = pcntl_fork())) {
 
 	require __DIR__."/shmop_helpers.php";
 
+	shell_exec("iptables -N TEA_FIREWALL >> /dev/null 2>&1");
+	shell_exec("iptables -A TEA_FIREWALL -j RETURN");
 	while (true) {
 		$shmid = shmop_open($shmKey[0], "c", 0600, SHMOP_SIZE);
 		$curData = str_from_mem(shmop_read($shmid, 0, SHMOP_SIZE));
@@ -94,6 +96,21 @@ if (!($blockerPid = pcntl_fork())) {
 		$r = json_decode($curData, true);
 		$curData = $r;
 		var_dump($r);
+
+		shell_exec("iptables -D TEA_FIREWALL -j RETURN");
+		printf("iptables -D TEA_FIREWALL -j RETURN\n");
+		foreach ($curData as $ip) {
+			
+			$a = sprintf("iptables -D TEA_FIREWALL -s %s -j DROP", $ip);
+			$b = sprintf("iptables -A TEA_FIREWALL -s %s -j DROP", $ip);
+
+			printf("%s\n%s\n", $a, $b);
+
+			shell_exec($a);
+			shell_exec($b);
+		}
+		printf("iptables -A TEA_FIREWALL -j RETURN\n");
+		shell_exec("iptables -A TEA_FIREWALL -j RETURN");
 		unset($r);
 
 		shmop_write($shmid, str_to_nts(json_encode($curData)), 0);
